bfd58e9f7ca8e2f3bee6ef118627e256cd98ef30
Vladislav-web <ykt.pc_master@mail.ru>
2021-09-25 12:13:45 +0300

    added function

--- js/practice-2.js
+++ js/practice-2.js
@@ -15,7 +15,7 @@
 'use strict';
 
 // 1
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 // 2 
 const personalMovieDB = {
@@ -27,18 +27,18 @@ const personalMovieDB = {
 };
 
 //3 
-
 for (let i = 0; i < 2; i++) {
   const a = prompt('Один из последних просмотренных фильмов?'),
         b = prompt('На сколько оцените его?');
-        personalMovieDB.movies[a] = b; 
-   if (a != null & b != null & a != '' & b != '' & a.length < 50) {
+        personalMovieDB.movies[a] = b;
+
+    if ( a != null & b != null & a != '' & b != '' & a.length < 50) {
       personalMovieDB.movies[a] = b;
       console.log('done');
-   } else {
-    console.log('error');
-    i--;
-   }
+    } else {
+      console.log('error');
+      i--;
+    }
 }
 
 if (personalMovieDB.count < 10) {
@@ -47,9 +47,10 @@ if (personalMovieDB.count < 10) {
   console.log("Вы классический зритель");
 } else if (personalMovieDB.count >= 30) {
   console.log("Вы киноман");
-} else {
-  console.log("Произошла ошибка");
+} else  {
+  console.log("Произошла ошибка"); 
 }
+
 
 console.log(personalMovieDB);
 
